541f3c712924e4087c7f3e84e198893526f8f89b
PravdeepK <118395938+PravdeepK@users.noreply.github.com>
2026-06-02 19:08:41 -0400

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,7 @@ const nextConfig: NextConfig = {
     localPatterns: [
       { pathname: "/logos/**" },
       { pathname: "/images/**" },
+      { pathname: "/company-logos/**" },
     ],
   },
 };
